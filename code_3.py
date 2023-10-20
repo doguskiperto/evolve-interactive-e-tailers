7b6e0d0f834f1ddae4d3b775a93f8f0c39ce1f6e
doguskiperto <144055162+doguskiperto@users.noreply.github.com>
2023-10-20 04:12:22 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+    data.forEach(item => {
+    const data = [];
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    }
+
+    return data;
+}
+
+function main() {
+    for (let i = 0; i < 10; i++) {
+
+}
+function generateRandomData() {
+    });
+        console.log("Random Number: " + item);
