5918b0b13845d1b8366475ebfdd99bfbf960086c
doguskiperto <144055162+doguskiperto@users.noreply.github.com>
2023-10-21 05:19:49 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,46 @@
+
+    return data;
+    const data = generateRandomData();
+    data.forEach(item => {
+function generateRandomData() {
+main();
+}
+    });
+    }
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+
+        console.log("Random Number: " + item);
+}
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    const data = [];
+}
+
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+
+    return data;
+}
+    }
+    });
+main();
+
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {import random
+        print(f"Random Number: {item}")
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+    for item in data:
+if __name__ == "__main__":
+
+def main():
+    data = generate_random_data()
+
+
+def generate_random_data():
