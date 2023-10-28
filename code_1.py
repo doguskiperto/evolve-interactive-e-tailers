bd3a54f8e5ed13f596a94dc544e1310c14414cdd
doguskiperto <144055162+doguskiperto@users.noreply.github.com>
2023-10-28 13:16:33 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,40 +1,14 @@
-    data = generate_random_data()
-
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-    main()
-if __name__ == "__main__":
-    for item in data:
-import random
-    return data
 
 
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
+
 if __name__ == "__main__":
 
-    main()
-
-def generate_random_data():
-def main():
-
-    data = generate_random_data()
-
-    return data
-    for item in data:
-        print(f"Random Number: {item}")if __name__ == "__main__":
-def generate_random_data():
-    return data
-    data = generate_random_data()
-    for item in data:
-
-
-def main():
-
-import random
         print(f"Random Number: {item}")
-
-    main()
+    data = generate_random_data()
+import random
+    for item in data:
+    return data
     data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    main()
+def main():
