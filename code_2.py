5284cf26456591f2c498638ec567f198df270cf3
doguskiperto <144055162+doguskiperto@users.noreply.github.com>
2023-10-29 13:24:17 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+    }
+
+        int[] data = new int[10];
+
+    public static void main(String[] args) {
+        }
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
